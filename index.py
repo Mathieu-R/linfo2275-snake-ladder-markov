93ebd443d215ff18e85d5095436b5c9881f9463b
Mathieu-R <mathieu0709@gmail.com>
2023-03-26 09:42:15 +0200

    comparison plot + click

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 import numpy as np
 import numpy.typing as npt
+import click
+
+from click.decorators import option
+from click.types import Choice
 
 from src.MarkovDecisionProcess import MarkovDecisionProcess
 from src.Simulation import Simulation
 
-from utils.plots import compare_costs
+from utils.plots import compare_costs_plot, compare_strategies_plot
 from utils.layouts import generate_layout, CUSTOM_LAYOUTS
 from utils.common import DICE, StrategyType
 
@@ -30,12 +34,130 @@ def markovDecision(layout: npt.NDArray, circle: bool = False) -> list[npt.NDArra
 	mdp.compute_transition_matrices()
 	results = mdp.launch_iteration_value()
 	return results
-	
-if __name__ == "__main__":
-	random_layout = generate_layout()
-	custom_layout = CUSTOM_LAYOUTS["JAILS_ON_FAST_LANE"]
-	circle = False
 
+def compare_costs(layout: npt.NDArray, best_dice, expected_costs, simulations: int, circle: bool):
+	# empirical simulation
+	simulation = Simulation(
+		layout=layout, 
+		dice=DICE,
+		circle=circle
+	)
+	empirical_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.OPTIMAL, 
+		number_of_simulations=simulations
+		)
+	print(f"Empirical cost for each cell: {empirical_costs}")
+
+	compare_costs_plot(
+		layout=layout,
+		theoretical_costs=expected_costs,
+		empirical_costs=empirical_costs,
+		title=f"Comparison of costs (circle={circle})"
+	)
+
+def compare_strategies(layout: npt.NDArray, best_dice, expected_costs, simulations: int, circle: bool):
+	# empirical simulation
+	simulation = Simulation(
+		layout=layout, 
+		dice=DICE,
+		circle=circle
+	)
+
+	security_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.SECURITY, 
+		number_of_simulations=simulations
+		)
+	
+	normal_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.NORMAL, 
+		number_of_simulations=simulations
+		)
+	
+	risky_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.RISKY, 
+		number_of_simulations=simulations
+		)
+
+	security_normal_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.SECURITY_NORMAL, 
+		number_of_simulations=simulations
+		)
+	
+	security_risky_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.SECURITY_RISKY, 
+		number_of_simulations=simulations
+		)
+	
+	normal_risky_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.NORMAL_RISKY, 
+		number_of_simulations=simulations
+		)
+	
+	random_costs = simulation.simulate(
+		best_dice=best_dice, 
+		strategy=StrategyType.RANDOM, 
+		number_of_simulations=simulations
+		)
+
+	suboptimal_costs = {
+		"security": security_costs,
+		"normal": normal_costs,
+		"security_normal": security_normal_costs,
+		"security_risky": security_risky_costs,
+		"normal_risky": normal_risky_costs,
+		"random": random_costs
+	}
+	
+	compare_strategies_plot(
+		layout=layout,
+		optimal_costs=expected_costs,
+		suboptimal_costs=suboptimal_costs,
+		title=f"Comparison with suboptimal strategies (circle={circle})"
+	)
+
+@click.command()
+@click.option(
+	"--layout", "-l",
+	type=click.Choice(["random", "NO_TRAPS", "JAILS_ON_FAST_LANE", "GAMBLE_EVERYWHERE", "NO_TRAPS_SLOW_LANE"]),
+	default="NO_TRAPS",
+	show_default=True,
+	help="Type of game board"
+)
+@click.option(
+	"--simulations", "-s",
+	is_flag=True,
+	default=10000,
+	show_default=True,
+	help="Number of simulations to run"
+)
+@click.option(
+	"--circle", "-c",
+	is_flag=True,
+	help="Make the board circle"
+)
+@click.option(
+	"--compare_costs", "-cc",
+	is_flag=True,
+	help="Show the relevance of MDP by empirical simulations."
+)
+@click.option(
+	"--compare_strategies", "-cs",
+	is_flag=True,
+	help="Compare the optimal strategy with suboptimal ones."
+)
+def main(layout, simulations, circle, compare_strategies):
+	if layout == "random":
+		custom_layout = generate_layout()
+	else:
+		custom_layout = CUSTOM_LAYOUTS[layout]
+	
 	# optimal strategy
 	result = markovDecision(layout=custom_layout, circle=circle)
 	expected_costs = result[0]
@@ -48,22 +170,22 @@ if __name__ == "__main__":
 	print(f"Expected cost for each cell: {expected_costs}")
 	print(f"Best die for each cell: {best_dice}")
 
-	# empirical simulation
-	simulation = Simulation(
-		layout=custom_layout, 
-		dice=DICE,
-		circle=circle
-	)
-	empirical_costs = simulation.simulate(
-		best_dice=best_dice, 
-		strategy=StrategyType.OPTIMAL, 
-		number_of_simulations=100000
+	if compare_costs:
+		compare_costs(
+			layout=custom_layout,
+			best_dice=best_dice,
+			expected_costs=expected_costs,
+			simulations=simulations,
+			circle=circle
 		)
-	print(f"Empirical cost for each cell: {empirical_costs}")
+	
+	elif compare_strategies:
+		compare_strategies(
 
-	compare_costs(
-		layout=custom_layout,
-		theoretical_costs=expected_costs,
-		empirical_costs=empirical_costs,
-		title=f"comparison of costs (circle={circle})"
-	)
+		)
+
+if __name__ == "__main__":
+	main()
+
+
+
--- utils/plots.py
+++ utils/plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from matplotlib import ticker
 
-def compare_costs(layout: npt.NDArray, theoretical_costs: npt.NDArray, empirical_costs: npt.NDArray, title: str):
+def compare_costs_plot(layout: npt.NDArray, theoretical_costs: npt.NDArray, empirical_costs: npt.NDArray, title: str):
 	fig, ax = plt.subplots(layout="constrained")
 
 	n = len(layout)
@@ -13,6 +13,27 @@ def compare_costs(layout: npt.NDArray, theoretical_costs: npt.NDArray, empirical
 
 	ax.bar(x, theoretical_costs, width=width, color="royalblue", align="edge", label="MDP cost")
 	ax.bar(x + width, empirical_costs, width=width, color="orange", align="edge", label="empirical cost")
+
+	ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+
+	ax.set_xlabel("State")
+	ax.set_ylabel("Expected cost")
+	ax.set_title(title)
+
+	ax.legend(loc="upper right", ncols=3)
+
+	plt.show()
+
+def compare_strategies_plot(layout: npt.NDArray, optimal_costs: npt.NDArray, suboptimal_costs: dict, title: str):
+	fig, ax = plt.subplots(layout="constrained")
+
+	n = len(layout)
+	x = np.arange(1, n)
+
+	ax.plot(x, optimal_costs, color="royalblue", label="optimal")
+
+	for (label, costs) in suboptimal_costs.items():
+		ax.plot(x, costs, label=label)
 
 	ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
 
